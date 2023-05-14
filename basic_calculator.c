e9064ac447615f69c56e9c557f3989dfe26534cc
SaintALaye <elitetechydev@gmail.com>
2023-05-14 01:13:26 +0100

    Handling Errors

--- basic_calculator.c
+++ basic_calculator.c
@@ -13,49 +13,60 @@ int main()
 	int num1, num2;
 	int result;
 	char operator[2];
+	char calc_again[1];
 
-	//iterating through the variable
-	do
+	while (calc_again[1] = 'y')
 	{
-		printf("Input the first number ");
+		//iterating through the variable
+		printf("Please type your first number... ");
 		scanf("%d", &num1);
-
+		
 		printf("Input the second number ");
 		scanf("%d", &num2);
 		
 		printf("Input the operator ");
 		scanf("%1s", operator);
-	}
-	while (num1 == 0 && num2 == 0 && operator[0] == 0);
 
-	if (operator[0] == '+')
-	{
-		result = num1 + num2;
-		printf("%d\n", result);
-	}
+		while (num1 == 0 && num2 == 0 && operator[0] == 0);
+	
+		if (operator[0] == '+')
+		{
+			result = num1 + num2;
+			printf("%d\n", result);
+		}
 
-	else if (operator[0] == '-')
-	{
-		result = num1 - num2;
-		printf("%d\n", result);
-	}
+		else if (operator[0] == '-')
+		{
+			result = num1 - num2;
+			printf("%d\n", result);
+		}
 
-	else if (operator[0] == '/')
-	{
-		result = num1 / num2;
-		printf("%d\n", result);
-	}
+		else if (operator[0] == '/')
+		{
+			result = num1 / num2;
+			printf("%d\n", result);
+		}
 
-	else if (operator[0] == '*')
-	{
-		result = num1 * num2;
-		printf("%d\n", result);
-	}
+		else if (operator[0] == '*')
+		{
+			result = num1 * num2;
+			printf("%d\n", result);
+		}
 
-	else
-	{
-		printf("Please select an operator\n");
-	}
+		else
+		{
+			printf("Please select an operator\n");
+		}
 
-	return 0;
+		printf("Do you want to calculate again? y/n... ");
+		scanf("%1s", calc_again);
+
+		if (calc_again[0] == 'n')
+		{
+			printf("Thank you...\n");
+			break;
+		}
+
+		return 0;
+	}
 }
