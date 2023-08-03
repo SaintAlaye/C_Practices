d8819d50cb53315b18287fb24973e73afaced8c3
SaintAlaye <elitetechydev@gmail.com>
2023-08-03 02:51:22 +0100

    First step to learning C

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - print hello world
+  * Return: 0 (EXIT_SUCCESS)
+  */
+int main(void)
+{
+	puts("Hello, World!");
+	return EXIT_SUCCESS;
+}
